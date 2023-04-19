00ca5b2eb690230563b7032196d2a4c74914d248
Syed Numan <111847221+syednuman2000@users.noreply.github.com>
2023-04-19 12:02:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let form = document.getElementById("form1");
+	let childList = form.children;
+	alert(childList[0].value+" "+childList[2].value);
 }
